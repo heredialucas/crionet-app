1230a5950241e36f7fd0ba5dc2c8a9ed3119bd4e
heredialucas <heredialucasfac22@gmail.com>
2024-11-26 19:54:59 +0100

    fix: hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apollo/config/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ApolloProvider client={client}>
         <App />
       </ApolloProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
